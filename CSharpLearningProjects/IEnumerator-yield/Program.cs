abdc01ff634adfe8cc4dde64efdaa74e9861a16c
Ahmed Kamal <ak417049@gmail.com>
2024-12-01 13:24:48 +0200

    Added C# learning projects to the solution

---
+++ CSharpLearningProjects/IEnumerator-yield/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq;
+namespace IEnumerator_yield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var intsss = new Fiveintegers(1, 2, 3, 4, 5);
+            foreach (var i in intsss)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        class Fiveintegers : IEnumerable    
+        {
+            int[] _values;
+            public Fiveintegers(int n1,int n2 ,int n3, int n4,int n5)
+            {
+                _values= new []{ n1,n2, n3,n4,n5};
+            }
+            public IEnumerator GetEnumerator()
+            {
+                foreach (var item in _values) 
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ CSharpLearningProjects/iEnumerator-example/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace iEnumerator_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee { Id = 100, Name = "Ahmed", Salary = 1000m, Department = "Cs" };
+            Employee e2= new Employee { Id = 100, Name = "Ahmed", Salary = 1000m, Department = "Cs" };
+            Employee e3= new Employee { Id = 100, Name = "Ahmed", Salary = 1000m, Department = "Cs" };
+
+           // Console.WriteLine(e1==e2); compare refence so false
+            Console.WriteLine(e1.Equals(e2)); //compare content but must over ride equals and gethashcode methods
+            Console.WriteLine(e2 == e3); // this compares content using the operator overload method
+
+
+        }
+        class Employee
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+            public decimal Salary { get; set; }
+            public string Department { get; set; }
+            public override bool Equals(object obj)
+            {
+             if(obj ==null || !(obj is Employee ))
+                    return false;
+             var emp = obj as Employee;
+
+                return this.Id == emp.Id
+                       && this.Name == emp.Name
+                       && this.Salary == emp.Salary
+                       && this.Department == emp.Department;
+            }
+
+            public override int GetHashCode()
+            {
+                var hash = 13;
+                hash = (hash * 7) + Id.GetHashCode();
+                hash = (hash * 7) + Name.GetHashCode();
+                hash = (hash * 7) + Salary.GetHashCode();
+                hash = (hash * 7) + Department.GetHashCode();
+                return base.GetHashCode();
+            }
+            public static bool operator ==(Employee left, Employee right) => left.Equals(right);
+            public static bool operator !=(Employee left, Employee right) => !left.Equals(right);
+
+
+
+        }
+    }
+}
